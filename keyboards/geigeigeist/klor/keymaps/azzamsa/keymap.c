71b1b0ac5bd3e6e6d15507819db58944cc3a31b4
Azzam S.A <noreply@azzamsa.com>
2024-09-06 23:51:15 +0700

    fix(klor): wrong thumb cluster

--- keyboards/geigeigeist/klor/keymaps/azzamsa/keymap.c
+++ keyboards/geigeigeist/klor/keymaps/azzamsa/keymap.c
@@ -55,45 +55,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT(
-             XXX,     XXX,     XXX,     XXX,     XXX,                KC_AGIN,  KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
-        XXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,                KC_CAPS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXX,
-        XXX, XXX,     XXX,     XXX,     XXX,     XXX, ___,  ___,     KC_INS,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXX,
-                               XXX,     XXX,     XXX, XXX,  CKC_ENT, CKC_BSPC, CKC_DEL, XXX
+             XXX,     XXX,     XXX,     XXX,     XXX,            KC_AGIN,  KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
+        XXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,            KC_CAPS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXX,
+        XXX, XXX,     XXX,     XXX,     XXX,     XXX, ___,  ___, KC_INS,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXX,
+                               XXX,     XXX,     XXX, XXX,  XXX, CKC_ENT, CKC_BSPC, CKC_DEL
     ),
 
     [_MOUSE] = LAYOUT(
-             XXX,     XXX,     XXX,     XXX,     XXX,                XXX,     XXX,     XXX,     XXX,     XXX,
-        XXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,                XXX,     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXX,
-        XXX, XXX,     XXX,     XXX,     XXX,     XXX, ___,  ___,     XXX,     KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXX,
-                               XXX,     XXX,     XXX, XXX,  KC_BTN3, KC_BTN1, KC_BTN2, XXX
+             XXX,     XXX,     XXX,     XXX,     XXX,            XXX,     XXX,     XXX,     XXX,     XXX,
+        XXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,            XXX,     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXX,
+        XXX, XXX,     XXX,     XXX,     XXX,     XXX, ___,  ___, XXX,     KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXX,
+                               XXX,     XXX,     XXX, XXX,  XXX, KC_BTN3, KC_BTN1, KC_BTN2
     ),
 
     [_MEDIA] = LAYOUT(
-            XXX,     XXX,      XXX,     XXX,     XXX,                XXX,     XXX,     XXX,     XXX,     XXX,
-       XXX, KC_LGUI, KC_LALT,  KC_LCTL, KC_LSFT, XXX,                XXX,     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXX,
-       XXX, XXX,     XXX,      XXX,     XXX,     XXX, ___,  ___,     XXX,     XXX,     XXX,     XXX,     XXX,     XXX,
-                               XXX,     XXX,     XXX, XXX,  KC_MSTP, KC_MPLY, KC_MUTE, XXX
+            XXX,     XXX,      XXX,     XXX,     XXX,            XXX,     XXX,     XXX,     XXX,     XXX,
+       XXX, KC_LGUI, KC_LALT,  KC_LCTL, KC_LSFT, XXX,            XXX,     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXX,
+       XXX, XXX,     XXX,      XXX,     XXX,     XXX, ___,  ___, XXX,     XXX,     XXX,     XXX,     XXX,     XXX,
+                               XXX,     XXX,     XXX, XXX,  XXX, KC_MSTP, KC_MPLY, KC_MUTE
     ),
 
     [_FUNC] = LAYOUT(
-            KC_F12, KC_F7, KC_F8, KC_F9,  KC_PSCR,               XXX, XXX,     XXX,     XXX,     XXX,
-       XXX, KC_F11, KC_F4, KC_F5, KC_F6,  KC_SCRL,               XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
-       XXX, KC_F10, KC_F1, KC_F2, KC_F3,  KC_PAUS, ___,     ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
-                           XXX,   KC_APP, KC_SPC,  KC_TAB,  XXX, XXX, XXX,     XXX
+            KC_F12, KC_F7, KC_F8,  KC_F9,  KC_PSCR,            XXX, XXX,     XXX,     XXX,     XXX,
+       XXX, KC_F11, KC_F4, KC_F5,  KC_F6,  KC_SCRL,            XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
+       XXX, KC_F10, KC_F1, KC_F2,  KC_F3,  KC_PAUS, ___,  ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
+                           KC_APP, KC_SPC, KC_TAB,  XXX,  XXX, XXX, XXX,     XXX
     ),
 
     [_NUM] = LAYOUT(
-             KC_LBRC, KC_7, KC_8, KC_9,   KC_RBRC,                XXX, XXX,     XXX,     XXX,     XXX,
-        XXX, KC_SCLN, KC_4, KC_5, KC_6,   KC_EQL,                 XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
-        XXX, KC_GRV,  KC_1, KC_2, KC_3,   KC_BSLS, ___,      ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
-                            XXX,  KC_DOT, KC_0,    KC_MINS,  XXX, XXX, XXX,     XXX
+             KC_LBRC, KC_7, KC_8,   KC_9, KC_RBRC,            XXX, XXX,     XXX,     XXX,     XXX,
+        XXX, KC_SCLN, KC_4, KC_5,   KC_6, KC_EQL,             XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
+        XXX, KC_GRV,  KC_1, KC_2,   KC_3, KC_BSLS, ___,  ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
+                            KC_DOT, KC_0, KC_MINS, XXX,  XXX, XXX, XXX,     XXX
     ),
 
     [_SYM] = LAYOUT(
-             KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                 XXX, XXX,     XXX,     XXX,     XXX,
-        XXX, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                 XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
-        XXX, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, ___,       ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
-                               XXX,     KC_LPRN, KC_RPRN, KC_UNDS,   XXX, XXX, XXX,     XXX
+             KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,            XXX, XXX,     XXX,     XXX,     XXX,
+        XXX, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,            XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
+        XXX, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, ___,  ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
+                               KC_LPRN, KC_RPRN, KC_UNDS, XXX,  XXX, XXX, XXX,     XXX
     ),
 
 };

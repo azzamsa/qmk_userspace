7fa09d1a76400b8f97622c3487b7e0a537dc379f
Azzam S.A <noreply@azzamsa.com>
2024-09-06 09:45:51 +0700

    fix(sofle): reflect the current miryoku layout

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                 XXX, XXX,     XXX,     XXX,     XXX,  XXX,
         XXX, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                 XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXX,
         XXX, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, ___,       ___, XXX, XXX,     XXX,     XXX,     XXX,     XXX,
-                      XXX,     XXX,     KC_GT,   KC_RPRN, KC_UNDS,   XXX, XXX, XXX,     XXX,     XXX
+                      XXX,     XXX,     KC_LPRN, KC_RPRN, KC_UNDS,   XXX, XXX, XXX,     XXX,     XXX
     ),
 
     [_LEFTG] = LAYOUT(

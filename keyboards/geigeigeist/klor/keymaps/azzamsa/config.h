a5e35a47679475b502ac33e9a86e63da3d6e1f0a
Azzam S.A <noreply@azzamsa.com>
2024-09-07 00:36:41 +0700

    fix(klor): try to increase the `SMTD_TIMEOUT_TAP`

--- keyboards/geigeigeist/klor/keymaps/azzamsa/config.h
+++ keyboards/geigeigeist/klor/keymaps/azzamsa/config.h
@@ -1,14 +1,6 @@
 #pragma once
 
 #define TAPPING_TERM 200
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
 
-// Auto Shift
-#define NO_AUTO_SHIFT_ALPHA
-#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-#define AUTO_SHIFT_NO_SETUP
-
-//
 // sm_td
 #define MAX_DEFERRED_EXECUTORS 10

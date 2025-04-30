c9e8b0e8a38f3795878fa63a70268d9eaa312fb6
Stanislav Markin <stasmarkin@gmail.com>
2025-04-30 08:12:37 -0300

    cheapino -> sm_td_0.5.0RC8

--- keyboards/cheapino/keymaps/azzamsa/keymap.c
+++ keyboards/cheapino/keymaps/azzamsa/keymap.c
@@ -10,34 +10,6 @@ enum layer_names {
     _SYM,
 };
 
-enum keycodes {
-    // SMTD keycodes begin
-    SMTD_KEYCODES_BEGIN = SAFE_RANGE,
-
-    // left home row
-    CKC_A,
-    CKC_R,
-    CKC_S,
-    CKC_T,
-
-    CKC_ESC,
-    CKC_SPC,
-    CKC_TAB,
-
-    // right home row
-    CKC_N,
-    CKC_E,
-    CKC_I,
-    CKC_O,
-
-    CKC_ENT,
-    CKC_BSPC,
-    CKC_DEL,
-
-    SMTD_KEYCODES_END,
-    // SMTD keycodes end
-};
-
 // include after enum def, otherwise it won't work.
 #include "features/sm_td.h"
 
@@ -49,17 +21,17 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_3(
       KC_Q,  KC_W,  KC_F,    KC_P,    KC_B,            KC_J,    KC_L,     KC_U,    KC_Y,   KC_QUOT,
-      CKC_A, CKC_R, CKC_S,   CKC_T,   KC_G,            KC_M,    CKC_N,    CKC_E,   CKC_I,  CKC_O,
+      KC_A,  KC_R,  KC_S,    KC_T,    KC_G,            KC_M,    KC_N,     KC_E,    KC_I,   KC_O,
       KC_Z,  KC_X,  KC_C,    KC_D,    KC_V,            KC_K,    KC_H,     KC_COMM, KC_DOT, KC_SLSH,
-                     CKC_ESC, CKC_SPC, CKC_TAB,        CKC_ENT, CKC_BSPC, CKC_DEL
-    ),
+                     KC_ESC,  KC_SPC,  KC_TAB,         KC_ENT,  KC_BSPC,  KC_DEL
+     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
       XXX,     XXX,     XXX,     XXX,      XXX,        KC_AGIN, KC_UNDO,  KC_CUT,  KC_COPY, KC_PSTE,
       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  XXX,        CW_TOGG, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT,
-      XXX,     CKC_DEL, CKC_ENT, CKC_BSPC, XXX,        KC_INS,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END,
-                        XXX,     XXX,      XXX,        CKC_ENT, CKC_BSPC, CKC_DEL
-    ),
+      XXX,     KC_DEL,  KC_ENT,  KC_BSPC,  XXX,        KC_INS,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END,
+                        XXX,     XXX,      XXX,        KC_ENT,  KC_BSPC,  KC_DEL
+     ),
 
     [_MOUSE] = LAYOUT_split_3x5_3(
       XXX,     XXX,     XXX,     XXX,     XXX,         XXX,     XXX,     XXX,     XXX,     XXX,
@@ -101,28 +73,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //
 // SMTD
 // https://github.com/stasmarkin/sm_td/
-void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
     switch (keycode) {
         // left side
-        SMTD_MT(CKC_A, KC_A, KC_LGUI)
-        SMTD_MT(CKC_R, KC_R, KC_LALT)
-        SMTD_MT(CKC_S, KC_S, KC_LCTL)
-        SMTD_MT(CKC_T, KC_T, KC_LSFT)
+        SMTD_MT(KC_A, KC_LGUI)
+        SMTD_MT(KC_R, KC_LALT)
+        SMTD_MT(KC_S, KC_LCTL)
+        SMTD_MT(KC_T, KC_LSFT)
 
-        SMTD_LT(CKC_ESC, KC_ESC, _MEDIA)
-        SMTD_LT(CKC_SPC, KC_SPACE, _NAV)
-        SMTD_LT(CKC_TAB, KC_TAB, _MOUSE)
+        SMTD_LT(KC_ESC, _MEDIA)
+        SMTD_LT(KC_SPACE, _NAV)
+        SMTD_LT(KC_TAB, _MOUSE)
 
         // righ side
-        SMTD_MT(CKC_N, KC_N, KC_LSFT)
-        SMTD_MT(CKC_E, KC_E, KC_LCTL)
-        SMTD_MT(CKC_I, KC_I, KC_LALT)
-        SMTD_MT(CKC_O, KC_O, KC_LGUI)
+        SMTD_MT(KC_N, KC_LSFT)
+        SMTD_MT(KC_E, KC_LCTL)
+        SMTD_MT(KC_I, KC_LALT)
+        SMTD_MT(KC_O, KC_LGUI)
 
-        SMTD_LT(CKC_ENT, KC_ENT, _SYM)
-        SMTD_LT(CKC_BSPC, KC_BSPC, _NUM)
-        SMTD_LT(CKC_DEL, KC_DEL, _FUNC)
+        SMTD_LT(KC_ENT, _SYM)
+        SMTD_LT(KC_BSPC, _NUM)
+        SMTD_LT(KC_DEL, _FUNC)
     }
+
+    return SMTD_RESOLUTION_UNHANDLED;
 }
 
 // Most of the keys were set to `300` and `20`, which I took from the sm_td author's keymaps.
@@ -130,28 +104,28 @@ void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
 uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
     switch (keycode) {
         // left side
-        case CKC_A:
+        case KC_A:
             if (timeout == SMTD_TIMEOUT_RELEASE) return 5; // default: 50
             break;
-        case CKC_R:
-        case CKC_S:
-        case CKC_T:
+        case KC_R:
+        case KC_S:
+        case KC_T:
 
-        case CKC_ESC:
-        case CKC_SPC:
-        case CKC_TAB:
+        case KC_ESC:
+        case KC_SPC:
+        case KC_TAB:
 
         // right side
-        case CKC_N:
-        case CKC_E:
-        case CKC_I:
-        case CKC_O:
+        case KC_N:
+        case KC_E:
+        case KC_I:
+        case KC_O:
             if (timeout == SMTD_TIMEOUT_RELEASE) return 5;
             break;
 
-        case CKC_ENT:
-        case CKC_BSPC:
-        case CKC_DEL:
+        case KC_ENT:
+        case KC_BSPC:
+        case KC_DEL:
    }
 
     return get_smtd_timeout_default(timeout);

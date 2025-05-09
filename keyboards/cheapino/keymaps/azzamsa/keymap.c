84290256fb012cde00859244e180ebdb068e4b65
Azzam S.A <noreply@azzamsa.com>
2025-05-09 20:05:07 +0700

    refactor(cheapino): curling the thumb is currently more comfortable than reaching
    Currently I am trying to use the outer thumb key than inner key.

--- keyboards/cheapino/keymaps/azzamsa/keymap.c
+++ keyboards/cheapino/keymaps/azzamsa/keymap.c
@@ -2,12 +2,14 @@
 
 #include "features/oneshot.h"
 
+// GUI
 #define HOME G(KC_LEFT)
 #define END G(KC_RGHT)
 #define FWD G(KC_RBRC)
 #define BACK G(KC_LBRC)
 #define TAB_L G(S(KC_LBRC))
 #define TAB_R G(S(KC_RBRC))
+// Alt
 #define SPACE_L A(G(KC_LEFT))
 #define SPACE_R A(G(KC_RGHT))
 // Momentarily
@@ -26,7 +28,7 @@ enum keycodes {
     OS_SHFT = SAFE_RANGE,
     OS_CTRL,
     OS_ALT,
-    OS_CMD,
+    OS_SUP,
 
     SW_WIN,  // Switch to next window         (cmd-tab)
     SW_LANG, // Switch to next input language (ctl-spc)
@@ -41,28 +43,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_F, KC_P,   KC_B,      KC_J,   KC_L,    KC_U,    KC_Y,   KC_QUOT,
         KC_A, KC_R, KC_S, KC_T,   KC_G,      KC_M,   KC_N,    KC_E,    KC_I,   KC_O,
         KC_Z, KC_X, KC_C, KC_D,   KC_V,      KC_K,   KC_H,    KC_COMM, KC_DOT, KC_SLSH,
-                    XXX,  KC_SPC, LA_NAV,    LA_SYM, KC_LSFT, XXX
+                    LA_NAV, KC_SPC, XXX,     XXX,    KC_LSFT, LA_SYM
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
         KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
         KC_COLN, KC_GRV, KC_LCBR, KC_RCBR, KC_PLUS,    KC_MINS, OS_SHFT, OS_CTRL, OS_ALT,  OS_SUP,
         KC_TILD, XXX,    XXX,     KC_SCLN, KC_PIPE,    KC_UNDS, KC_BSLS, KC_AMPR, KC_QUES, KC_SLASH,
-                         XXX,     ___,     ___,        ___,     ___,     XXX
+                         ___,     ___,     XXX,        XXX,  ___,     ___
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        QK_BOOT, SW_WIN,  TAB_L,  TAB_R,  KC_VOLU,    KC_TAB,  KC_ESC,  KC_ENT,  KC_BSPC, KC_DEL,
-        OS_SHFT, OS_CTRL, OS_ALT, OS_SUP, KC_VOLD,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        SPACE_L, SPACE_R, BACK,   FWD,    KC_MPLY,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                          XXX,    ___,    ___,        ___,     ___,     XXX
+        QK_BOOT, SW_WIN,  TAB_L,   TAB_R,   KC_VOLU,    KC_TAB,  KC_ESC,  KC_ENT,  KC_BSPC, KC_DEL,
+        OS_SUP,  OS_ALT,  OS_CTRL, OS_SHFT, KC_VOLD,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        SPACE_L, SPACE_R, BACK,    FWD,     KC_MPLY,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+                          ___,     ___,     XXX,        XXX,  ___,     ___
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_1,    KC_2,    KC_3,   KC_4,   KC_5,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,
-        OS_SHFT, OS_CTRL, OS_ALT, OS_SUP, KC_F11,    KC_F12, OS_SHFT, OS_CTRL, OS_ALT,  OS_SUP,
-        KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,     KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,
-                          XXX,    ___,    ___,       ___,    ___,     XXX
+        KC_1,   KC_2,   KC_3,    KC_4,    KC_5,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,
+        OS_SUP, OS_ALT, OS_CTRL, OS_SHFT, KC_F11,    KC_F12, OS_SHFT, OS_CTRL, OS_ALT,  OS_SUP,
+        KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,     KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,
+                          ___,   ___,     XXX,       XXX,    ___,     ___
     ),
 };
 

3d543e42e1419f768d259f7fa0d38f3e6097854b
Azzam S.A <noreply@azzamsa.com>
2025-05-31 19:12:00 +0700

    refactor(keymap): better symbol position
    I don't need `del word` as it is super easy to do in Miryoku layout.

--- keyboards/cheapino/keymaps/azzamsa/keymap.c
+++ keyboards/cheapino/keymaps/azzamsa/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_split_3x5_3(
       XXX,     XXX,     XXX,     XXX,      XXX,        KC_AGIN, PASTE,    COPY,    CUT,     UNDO,
       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  XXX,        CW_TOGG, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT,
-      XXX,     XXX,     XXX,     XXX,      XXX,        DL_WORD, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,
+      XXX,     XXX,     XXX,     XXX,      XXX,        KC_INS, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,
                         XXX,     XXX,      XXX,        KC_ENT,  KC_BSPC,  KC_DEL
      ),
 
@@ -72,15 +72,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYM] = LAYOUT_split_3x5_3(
       KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_AMPR,        XXX, XXX,     XXX,     XXX,     XXX,
-      KC_GRV,   KC_COLN, KC_LCBR, KC_LBRC, KC_UNDS,        XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+      KC_GRV,   KC_COLN, KC_LCBR, KC_LBRC, KC_CIRC,        XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
       KC_TILDE, KC_SCLN, KC_RCBR, KC_RBRC, KC_PIPE,        XXX, XXX,     XXX,     XXX,     XXX,
                          KC_LPRN, KC_RPRN, KC_UNDS,        XXX, XXX,     XXX
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-      KC_EQL,  KC_7, KC_8,    KC_9, KC_PLUS,              XXX, XXX,     XXX,     XXX,     XXX,
+      XXX,     KC_7, KC_8,    KC_9, KC_PLUS,              XXX, XXX,     XXX,     XXX,     XXX,
       KC_ASTR, KC_4, KC_5,    KC_6, KC_EQL,               XXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-      KC_CIRC, KC_1, KC_2,    KC_3, KC_BSLS,              XXX, XXX,     XXX,     XXX,     XXX,
+      XXX,     KC_1, KC_2,    KC_3, KC_BSLS,              XXX, XXX,     XXX,     XXX,     XXX,
                      KC_PERC, KC_0, KC_MINS,              XXX, XXX,     XXX
     ),
 

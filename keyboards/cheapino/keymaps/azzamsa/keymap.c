05e775ea8a0b6f7530c03e747843362e8b9ef57e
Azzam S.A <noreply@azzamsa.com>
2025-11-23 08:00:54 +0700

    build(cheapino): upgrade to latest qmk

--- keyboards/cheapino/keymaps/azzamsa/keymap.c
+++ keyboards/cheapino/keymaps/azzamsa/keymap.c
@@ -26,6 +26,10 @@
 #define MT_I   LALT_T(KC_I)
 #define MT_O   LGUI_T(KC_O)
 
+// Night
+#define MT_H   LCTL_T(KC_H)
+#define MT_C   LSFT_T(KC_C)
+
 // Other aliases
 #define DL_WORD  LCTL(KC_BSPC)
 #define REDO     KC_AGIN
@@ -76,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      ),
 
     [_MOUSE] = LAYOUT_split_3x5_3(
-      XXX,     XXX,     XXX,     XXX,     XXX,         XXX,     KC_BTN4, XXX,     XXX,     KC_BTN5,
-      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,         XXX,     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-      XXX,     XXX,     XXX,     XXX,     XXX,         XXX,     KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-                        XXX,     XXX,     XXX,         KC_BTN3, KC_BTN1, KC_BTN2
+      XXX,     XXX,     XXX,     XXX,     XXX,         XXX,     MS_BTN4, XXX,     XXX,     MS_BTN5,
+      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXX,         XXX,     MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT,
+      XXX,     XXX,     XXX,     XXX,     XXX,         XXX,     MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR,
+                        XXX,     XXX,     XXX,         MS_BTN3, MS_BTN1, MS_BTN2
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
@@ -120,7 +124,7 @@ const uint16_t PROGMEM tab_combo[]     = { KC_W, KC_SLSH,    COMBO_END };
 
 combo_t key_combos[] = {
   [REISUB] = COMBO_ACTION(reisub_combo),
-  [TAB_COMBO] = COMBO(tab_combo, KC_TAB),
+  [TAB]    = COMBO(tab_combo, KC_TAB),
 };
 
 

f71a2d52ad71d9291c895736450aac1cc3254ae1
Azzam S.A <noreply@azzamsa.com>
2025-11-25 09:54:44 +0700

    fix(cheapino): HRM doesn't work as usual
    trying to set 230

--- keyboards/cheapino/keymaps/azzamsa/config.h
+++ keyboards/cheapino/keymaps/azzamsa/config.h
@@ -3,9 +3,16 @@
 #define BOOTMAGIC_ROW 0
 #define BOOTMAGIC_COLUMN 1
 
-// homerow modifiers
+//
+// Home-Row Modifiers
+
+// TAPPING_TERM = max time (ms) between press and release to count as a *tap*.
+// Higher value  → you can tap slower (more forgiving).
+// Lower value   → you must tap faster (snappier, but easier to mis-trigger).
+// Increase if taps become holds; decrease if holds feel delayed.
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
-#define FLOW_TAP_TERM 150
-#define CHORDAL_HOLD
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 230
+
+/* #define FLOW_TAP_TERM 150 */
+/* #define CHORDAL_HOLD */
+/* #define PERMISSIVE_HOLD */

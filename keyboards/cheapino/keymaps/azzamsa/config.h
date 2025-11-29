9c40a4cfee30fff0aa9c8c8e1f470ceed5255b27
Azzam S.A <noreply@azzamsa.com>
2025-11-29 14:42:53 +0700

    fix(cheapino): HRM doesn't work as usual
    I have been using this for 4 days, and it feels better.

--- keyboards/cheapino/keymaps/azzamsa/config.h
+++ keyboards/cheapino/keymaps/azzamsa/config.h
@@ -10,9 +10,14 @@
 // Higher value  → you can tap slower (more forgiving).
 // Lower value   → you must tap faster (snappier, but easier to mis-trigger).
 // Increase if taps become holds; decrease if holds feel delayed.
-#undef TAPPING_TERM
-#define TAPPING_TERM 230
 
-/* #define FLOW_TAP_TERM 150 */
-/* #define CHORDAL_HOLD */
-/* #define PERMISSIVE_HOLD */
+#undef TAPPING_TERM
+#define TAPPING_TERM 200 // default
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
